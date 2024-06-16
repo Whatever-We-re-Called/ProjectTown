da67db39d3771fddc846824e0b8f2769da95e2cc
Luicel <luicelcs@gmail.com>
2024-06-16 18:09:34 -0500

    add interaction manager

--- common/interactions/interaction_area.gd
+++ common/interactions/interaction_area.gd
@@ -1,7 +1,24 @@
 class_name InteractionArea extends Area3D
 
 @export var action_name: String = "Interact"
+@export var label_y_offset: float = 0
 
 var interact_callable: Callable = func():
 	pass
 
+
+func _ready():
+	collision_layer = 0
+	collision_mask = 2
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+
+func _on_body_entered(body):
+	print("enter")
+	InteractionManager.register_area(self)
+
+
+func _on_body_exited(body):
+	print("exit")
+	InteractionManager.unregister_area(self)
---
+++ common/interactions/interaction_manager.gd
@@ -0,0 +1,49 @@
+# Initially yoinked from https://youtu.be/ajCraxGAeYU?si=wkLHIG_Buhvy_PVt.
+extends Node3D
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var interaction_sprite = %InteractionSprite
+@onready var interact_label = %InteractLabel
+
+const BASE_TEXT = "[E] "
+
+var active_areas = []
+var can_interact = true
+
+
+func _process(delta):
+	if active_areas.size() > 0 and can_interact:
+		active_areas.sort_custom(_sort_by_distance_to_player)
+		interact_label.text = BASE_TEXT + active_areas[0].action_name
+		interaction_sprite.global_position = active_areas[0].global_position
+		interaction_sprite.global_position.y += active_areas[0].label_y_offset
+		interaction_sprite.rotation_degrees.x = -45.0
+		interaction_sprite.show()
+	else:
+		interaction_sprite.hide()
+
+
+func _sort_by_distance_to_player(area1: InteractionArea, area2: InteractionArea):
+	var area1_to_player = player.global_position.distance_to(area1.global_position)
+	var area2_to_player = player.global_position.distance_to(area2.global_position)
+	return area1_to_player < area2_to_player
+
+
+func register_area(area: InteractionArea):
+	active_areas.push_back(area)
+
+
+func unregister_area(area: InteractionArea):
+	var index = active_areas.find(area)
+	if index != -1:
+		active_areas.remove_at(index)
+
+
+func _input(event):
+	if event.is_action_pressed("interact") and can_interact:
+		if active_areas.size() > 0:
+			can_interact = false
+			interact_label.hide()
+			await active_areas[0].interact.call()
+			
+			can_interact = true
